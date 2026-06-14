726c04192c410d0e62e9420f571b229b97b6e5b6
Jesse Nickles <jesse@littlebizzy.com>
2026-06-14 13:56:04 +0700

    Add strobe speed preset helper

---
+++ app/src/main/java/top/thinapps/brightflashlight/torch/StrobeSpeedPreset.kt
@@ -0,0 +1,34 @@
+package top.thinapps.brightflashlight.torch
+
+object StrobeSpeedPreset {
+    const val DEFAULT_HZ = 2
+
+    fun hzForSliderValue(value: Int): Int {
+        return when (value.coerceIn(0, 4)) {
+            0 -> 1
+            1 -> 2
+            2 -> 3
+            3 -> 4
+            else -> 6
+        }
+    }
+
+    fun sliderValueForHz(hz: Int): Int {
+        return when {
+            hz <= 1 -> 0
+            hz == 2 -> 1
+            hz == 3 -> 2
+            hz == 4 -> 3
+            else -> 4
+        }
+    }
+
+    fun normalizeHz(hz: Int): Int {
+        return hzForSliderValue(sliderValueForHz(hz))
+    }
+
+    fun intervalMsForHz(hz: Int): Long {
+        val periodMs = (1000.0 / normalizeHz(hz).toDouble()).toLong()
+        return periodMs.coerceAtLeast(30L)
+    }
+}
